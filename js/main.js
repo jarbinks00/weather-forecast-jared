3bd54baa88f94dee5d1ea1795f0ed809e35d4df3
jarbinks00 <jaredjj869@gmail.com>
2024-05-25 19:10:27 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+        // sticky header
+        function sticky_header(){
+            var wind = $(window);
+            var sticky = $('header');
+            wind.on('scroll', function () {
+                var scroll = wind.scrollTop();
+                if (scroll < 100) {
+                    sticky.removeClass('sticky');
+                } else {
+                    sticky.addClass('sticky');
+                }
+            });
+        }
+        sticky_header();
+        //===== Back to top
+
+        // Show or hide the sticky footer button
+        $(window).on('scroll', function () {
+            if ($(this).scrollTop() > 600) {
+                $('.back-to-top').fadeIn(200)
+            } else {
+                $('.back-to-top').fadeOut(200)
+            }
+        });
+
+        //Animate the scroll to yop
+        $('.back-to-top').on('click', function (event) {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: 0,
+            }, 900);
+        });
+
+    });
+
+})(jQuery);
